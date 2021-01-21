15a5d133136881e193211316aaf1047ad77cb06b
David Manzer <david.manzere@softrams.com>
2021-01-21 15:29:11 -0700

    more dev on recipe

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,6 +9,7 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent implements OnInit {
 
   recipes: Recipe[] = [
+    new Recipe('A test recipe', 'This is a test', 'https://i.pinimg.com/originals/7f/5d/08/7f5d0818893b7151b4180d2ced804b25.jpg'),
     new Recipe('A test recipe', 'This is a test', 'https://i.pinimg.com/originals/7f/5d/08/7f5d0818893b7151b4180d2ced804b25.jpg')
   ];
 

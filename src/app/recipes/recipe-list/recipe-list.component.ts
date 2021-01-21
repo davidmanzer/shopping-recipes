fb2f0d7cd10febff3f0c86bcdf2c3c9083f7fb01
David Manzer <david.manzere@softrams.com>
2021-01-21 10:58:55 -0700

    more dev on the recipe

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
+  recipes: Recipe[] = [
+    new Recipe('A test recipe', 'This is a test', 'https://i.pinimg.com/originals/7f/5d/08/7f5d0818893b7151b4180d2ced804b25.jpg')
+  ];
+
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
 
 }
